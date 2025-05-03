0092b7b6362349295953293d93db7291ad3687c7
chris Taylor <ctay456a@gmail.com>
2025-05-03 04:15:15 -0400

    Create TicketRepository.java

---
+++ src/main/java/com/example/ticketing/repository/TicketRepository.java
@@ -0,0 +1,6 @@
+package com.example.ticketing.repository;
+
+import com.example.ticketing.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {}
