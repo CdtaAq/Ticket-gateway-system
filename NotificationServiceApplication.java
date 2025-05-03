8e06e65f831fb9dc956262c1e6176bf9f5c56a91
chris Taylor <ctay456a@gmail.com>
2025-05-03 01:13:54 -0400

    Create NotificationServiceApplication.java

---
+++ NotificationServiceApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class NotificationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
