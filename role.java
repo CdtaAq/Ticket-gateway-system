c7a46bbe7941b415f0bbbc0feb4aa8437fb4f043
chris Taylor <ctay456a@gmail.com>
2025-05-07 12:51:34 -0400

    Update role.java

--- role.java
+++ role.java
@@ -1,8 +1,39 @@
-@Entity
-public class Role {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-    @Column(unique = true)
-    private String name;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    public Optional<Role> getRoleById(Long id) {
+        return roleRepository.findById(id);
+    }
+
+    public Role saveRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    public Role updateRole(Long id, Role role) {
+        return roleRepository.findById(id)
+                .map(existingRole -> {
+                    existingRole.setName(role.getName());
+                    return roleRepository.save(existingRole);
+                })
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+    }
+
+    public void deleteRole(Long id) {
+        roleRepository.deleteById(id);
+    }
 }
