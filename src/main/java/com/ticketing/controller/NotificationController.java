c0bfe84c62e00ebaf9e33e87bbbf8af49f40f8ad
chris Taylor <ctay456a@gmail.com>
2025-05-11 02:10:36 -0400

    Create NotificationController.java

---
+++ src/main/java/com/ticketing/controller/NotificationController.java
@@ -0,0 +1,23 @@
+package com.ticketing.controller;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public NotificationController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @PostMapping("/send")
+    public void sendNotification(@RequestBody String message) {
+        messagingTemplate.convertAndSend("/topic/notifications", message);
+    }
+}
